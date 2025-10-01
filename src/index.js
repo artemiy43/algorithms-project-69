103b89e11e422976650c7ac0076dc5253d1f5a09
artemiy.bondar <artemiy.bondar@simpleone.ru>
2025-10-01 17:38:47 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,81 @@
+const search = (documents, query) => {
+    const terms = query.match(/\w+/g);
+    const invertedDocuments = getInvertIndex(documents);
+    const result = [...findNeededDocs(terms, invertedDocuments)];
+    const sortedResult = sort(result, documents,invertedDocuments, terms);
+    return sortedResult;
+};
+
+const getInvertIndex = (docs) => {
+    const result = {};
+    const words = new Set();
+    docs.forEach(doc => {
+        doc.text.match(/\w+/g).forEach(word => {
+            words.add(word);
+        });
+    });
+
+    for (let word of words) {
+        if (!result[word]) {
+            result[word] = [];
+        }
+
+        for (let doc of docs) {
+            doc.text.match(/\w+/g).forEach(docWord => {
+                if (docWord === word) {
+                    result[word].push(doc.id);
+                }
+            })
+        }
+    }
+
+    return result;
+};
+
+const findNeededDocs = (terms, invertedDocuments) => {
+    const result = new Set();
+    for (let term of terms) {
+        if (invertedDocuments[term]) {
+            for (doc of invertedDocuments[term]) {
+                result.add(doc);
+            }
+        }
+    }
+
+    return result;
+};
+
+const sort = (result, documents, invertedDocuments, terms) => {
+    const collection = {};
+    const countOfDocuments = documents.length;
+
+    for (let doc of documents) {
+        collection[doc.id] = {};
+        collection[doc.id].INDEX = 0;
+    }
+
+    for (let term of terms) {
+        for (let doc of documents) {
+            const tf = tfCalculate(term, doc, invertedDocuments);
+            const idf = idfCalculate(countOfDocuments, term, invertedDocuments);
+
+            collection[doc.id].INDEX += tf * idf;
+        }
+    }
+
+    return Object.entries(collection)
+    .filter(entity => result.includes(entity[0]))
+    .sort((a,b)=> collection[b[0]].INDEX - collection[a[0]].INDEX)
+    .map(entity => entity[0]);
+};
+
+const tfCalculate = (term, doc, invertedDocuments) => {
+    return (invertedDocuments[term].filter(invertedDoc => invertedDoc === doc.id).length) / doc.text.match(/\w+/g).length;
+};
+
+const idfCalculate = (countOfDocuments, term, invertedDocuments) => {
+    const set = new Set(invertedDocuments[term]);
+    return Math.log((countOfDocuments) / Number(set.size));
+};
+
+export default search;
