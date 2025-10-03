bbb9af9856dcca5ab36d221710cb6776e70123b8
artemiy.bondar <artemiy.bondar@simpleone.ru>
2025-10-03 14:17:04 +0300

    fix lint 3

--- src/index.js
+++ src/index.js
@@ -8,28 +8,28 @@ const getInvertIndex = (docs) => {
     });
   });
 
-  words.forEach(word => {
+  words.forEach((word) => {
     if (!result[word]) {
-        result[word] = [];
+      result[word] = [];
     }
 
-    docs.forEach(doc => {
-        doc.text.toLowerCase().match(/\w+/g).forEach((docWord) => {
-            if (docWord === word) {
-                result[word].push(doc.id);
-            }
-        });
+    docs.forEach((doc) => {
+      doc.text.toLowerCase().match(/\w+/g).forEach((docWord) => {
+          if (docWord === word) {
+            result[word].push(doc.id);
+          }
+      });
     });
-  }); 
+  });
 
   return result;
 };
 
 const findNeededDocs = (terms, invertedDocuments) => {
   const result = new Set();
-  terms.forEach(term => {
+  terms.forEach((term) => {
     if (invertedDocuments[term]) {
-      invertedDocuments[term].forEach(doc => {
+      invertedDocuments[term].forEach((doc) => {
         result.add(doc);
       });
     }
@@ -38,9 +38,7 @@ const findNeededDocs = (terms, invertedDocuments) => {
   return result;
 };
 
-const tfCalculate = (term, doc, invertedDocuments) => {
-  return invertedDocuments[term] ? (invertedDocuments[term].filter(invertedDoc => invertedDoc === doc.id).length) / doc.text.toLowerCase().match(/\w+/g).length : 0;
-};
+const tfCalculate = (term, doc, invertedDocuments) => invertedDocuments[term] ? (invertedDocuments[term].filter((invertedDoc) => invertedDoc === doc.id).length) / doc.text.toLowerCase().match(/\w+/g).length : 0;
 
 const idfCalculate = (countOfDocuments, term, invertedDocuments) => {
   const set = invertedDocuments[term] ? new Set(invertedDocuments[term]) : new Set();
@@ -51,13 +49,13 @@ const sort = (result, documents, invertedDocuments, terms) => {
   const collection = {};
   const countOfDocuments = documents.length;
 
-  documents.forEach(doc => {
+  documents.forEach((doc) => {
     collection[doc.id] = {};
     collection[doc.id].INDEX = 0;
   });
 
-  terms.forEach(term => {
-    documents.forEach(doc => {
+  terms.forEach((term) => {
+    documents.forEach((doc) => {
       const tf = tfCalculate(term, doc, invertedDocuments);
       const idf = idfCalculate(countOfDocuments, term, invertedDocuments);
 
@@ -66,9 +64,9 @@ const sort = (result, documents, invertedDocuments, terms) => {
   });
 
   const answer = Object.entries(collection)
-    .filter(entity => result.includes(entity[0]) && entity[1] !== 0)
+    .filter((entity) => result.includes(entity[0]) && entity[1] !== 0)
     .sort((a, b) => collection[b[0]].INDEX - collection[a[0]].INDEX)
-    .map(entity => entity[0]);
+    .map((entity) => entity[0]);
 
   return answer;
 };
