7da3f3f039164d0a4d2e5364a080a6a7340b490a
artemiy.bondar <artemiy.bondar@simpleone.ru>
2025-10-03 12:47:39 +0300

    fix lint and remove console.log

--- __tests__/search.test.js
+++ __tests__/search.test.js
@@ -1,39 +1,36 @@
-import search from '../index.js'
 import { expect, test } from '@jest/globals'
+import search from '../index.js'
 
 test('should find documents containing the string', () => {
   const docs = [
     { id: 'doc1', text: 'hello world' },
-    { id: 'doc2', text: 'goodbye' }
-  ];
-  expect(search(docs, 'hello')).toEqual(['doc1']);
-});
+    { id: 'doc2', text: 'goodbye' },
+  ]
+  expect(search(docs, 'hello')).toEqual(['doc1'])
+})
 
 test('should find documents containing the string with !', () => {
-  const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
-  const docs = [doc1];
+  const doc1 = { id: 'doc1', text: 'I can\'t shoot straight unless I\'ve had a pint!' }
+  const docs = [doc1]
 
-  // Теперь наш поиск может находить слова, независимо от знаков препинания
-  expect(search(docs, 'pint')).toEqual(['doc1']);
-  expect(search(docs, 'pint!')).toEqual(['doc1']);
-});
+  expect(search(docs, 'pint')).toEqual(['doc1'])
+  expect(search(docs, 'pint!')).toEqual(['doc1'])
+})
 
 test('with relevant', () => {
-  const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
-  const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
-  const doc3 = { id: 'doc3', text: "I'm your shooter." };
-  const docs = [doc1, doc2, doc3];
+  const doc1 = { id: 'doc1', text: 'I can\'t shoot straight unless I\'ve had a pint!' }
+  const doc2 = { id: 'doc2', text: 'Don\'t shoot shoot shoot that thing at me.' }
+  const doc3 = { id: 'doc3', text: 'I\'m your shooter.' }
+  const docs = [doc1, doc2, doc3]
 
-  // Теперь наш поиск может находить слова, независимо от знаков препинания
-  expect(search(docs, 'shoot')).toEqual(['doc2', 'doc1']);
-});
+  expect(search(docs, 'shoot')).toEqual(['doc2', 'doc1'])
+})
 
 test('with several words', () => {
-  const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
-  const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
-  const doc3 = { id: 'doc3', text: "I'm your shooter." };
-  const docs = [doc1, doc2, doc3];
+  const doc1 = { id: 'doc1', text: 'I can\'t shoot straight unless I\'ve had a pint!' }
+  const doc2 = { id: 'doc2', text: 'Don\'t shoot shoot shoot that thing at me.' }
+  const doc3 = { id: 'doc3', text: 'I\'m your shooter.' }
+  const docs = [doc1, doc2, doc3]
 
-  // Теперь наш поиск может находить слова, независимо от знаков препинания
-  expect(search(docs, 'shoot at me')).toEqual(['doc2', 'doc1']);
-});
+  expect(search(docs, 'shoot at me')).toEqual(['doc2', 'doc1'])
+})
--- src/index.js
+++ src/index.js
@@ -1,101 +1,88 @@
-const search = (documents, query) => {
-    if (!documents || !query) {
-        return [];
-    }
-
-    const terms = query.toLowerCase().match(/\w+/g);
-    const invertedDocuments = getInvertIndex(documents);
-    const result = [...findNeededDocs(terms, invertedDocuments)];
-    const sortedResult = sort(result, documents,invertedDocuments, terms);
-    return sortedResult;
-};
-
 const getInvertIndex = (docs) => {
-    const result = {};
-    const words = new Set();
+  const result = {}
+  const words = new Set()
 
-    docs.forEach(doc => {
-        doc.text.toLowerCase().match(/\w+/g).forEach(word => {
-            words.add(word);
-        });
-    });
+  docs.forEach((doc) => {
+    doc.text.toLowerCase().match(/\w+/g).forEach((word) => {
+      words.add(word)
+    })
+  })
 
-    for (let word of words) {
-        if (!result[word]) {
-            result[word] = [];
-        }
-
-        for (let doc of docs) {
-            doc.text.toLowerCase().match(/\w+/g).forEach(docWord => {
-                if (docWord === word) {
-                    result[word].push(doc.id);
-                }
-            })
-        }
+  for (const word of words) {
+    if (!result[word]) {
+      result[word] = []
     }
 
-    return result;
-};
+    for (const doc of docs) {
+      doc.text.toLowerCase().match(/\w+/g).forEach((docWord) => {
+        if (docWord === word) {
+          result[word].push(doc.id)
+        }
+      })
+    }
+  }
+
+  return result
+}
 
 const findNeededDocs = (terms, invertedDocuments) => {
-    const result = new Set();
-    for (let term of terms) {
-        if (invertedDocuments[term]) {
-            for (let doc of invertedDocuments[term]) {
-                result.add(doc);
-            }
-        }
+  const result = new Set()
+  for (const term of terms) {
+    if (invertedDocuments[term]) {
+      for (const doc of invertedDocuments[term]) {
+        result.add(doc)
+      }
     }
+  }
 
-    return result;
-};
+  return result
+}
 
 const sort = (result, documents, invertedDocuments, terms) => {
-    const collection = {};
-    const countOfDocuments = documents.length;
-    console.log(JSON.stringify(countOfDocuments));
+  const collection = {}
+  const countOfDocuments = documents.length
 
-    for (let doc of documents) {
-        collection[doc.id] = {};
-        collection[doc.id].INDEX = 0;
+  for (const doc of documents) {
+    collection[doc.id] = {}
+    collection[doc.id].INDEX = 0
+  }
+
+  for (const term of terms) {
+    for (const doc of documents) {
+      const tf = tfCalculate(term, doc, invertedDocuments)
+      const idf = idfCalculate(countOfDocuments, term, invertedDocuments)
+
+      collection[doc.id].INDEX += tf * idf
     }
+  }
 
-    for (let term of terms) {
-        for (let doc of documents) {
-            console.log(JSON.stringify(`term: ${term}`));
-            console.log(JSON.stringify(`doc: ${JSON.stringify(doc)}`));
-
-            const tf = tfCalculate(term, doc, invertedDocuments);
-            const idf = idfCalculate(countOfDocuments, term, invertedDocuments);
-
-            console.log(JSON.stringify(`tf: ${tf}`));
-            console.log(JSON.stringify(`idf: ${idf}`));
-
-            collection[doc.id].INDEX += tf * idf;
-        }
-    }
-
-    console.log(JSON.stringify(collection));
-
-    const answer = Object.entries(collection)
+  const answer = Object.entries(collection)
     .filter(entity => result.includes(entity[0]) && entity[1] !== 0)
-    .sort((a,b)=> collection[b[0]].INDEX - collection[a[0]].INDEX)
-    .map(entity => entity[0]);
+    .sort((a, b) => collection[b[0]].INDEX - collection[a[0]].INDEX)
+    .map(entity => entity[0])
 
-    console.log(JSON.stringify(`answer: ${answer}`));
-
-    return answer;
-};
+  return answer
+}
 
 const tfCalculate = (term, doc, invertedDocuments) => {
-    return invertedDocuments[term] ? (invertedDocuments[term].filter(invertedDoc => invertedDoc === doc.id).length) / doc.text.toLowerCase().match(/\w+/g).length : 0;
-};
+  return invertedDocuments[term] ? (invertedDocuments[term].filter(invertedDoc => invertedDoc === doc.id).length) / doc.text.toLowerCase().match(/\w+/g).length : 0
+}
 
 const idfCalculate = (countOfDocuments, term, invertedDocuments) => {
-    const set = invertedDocuments[term] ? new Set(invertedDocuments[term]) : new Set();
-    console.log(JSON.stringify(`countOfDocuments: ${countOfDocuments}`));
-    console.log(JSON.stringify(`set.size: ${set.size}`));
-    return Math.log2(1 + (countOfDocuments - set.size + 1) / (set.size + 0.5));
-};
+  const set = invertedDocuments[term] ? new Set(invertedDocuments[term]) : new Set()
+  return Math.log2(1 + (countOfDocuments - set.size + 1) / (set.size + 0.5))
+}
 
-export default search;
+const search = (documents, query) => {
+  if (!documents || !query) {
+    return []
+  }
+
+  const terms = query.toLowerCase().match(/\w+/g)
+  const invertedDocuments = getInvertIndex(documents)
+  const result = [...findNeededDocs(terms, invertedDocuments)]
+  const sortedResult = sort(result, documents, invertedDocuments, terms)
+  return sortedResult
+}
+
+export default search
