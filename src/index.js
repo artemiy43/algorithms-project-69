5c8be9f02fdad2be5d3f8af7853b0e4553af3189
artemiy.bondar <artemiy.bondar@simpleone.ru>
2025-10-03 11:08:35 +0300

    fix12

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const search = (documents, query) => {
-    if (!documents) {
+    if (!documents || !query) {
         return [];
     }
 
@@ -53,7 +53,6 @@ const findNeededDocs = (terms, invertedDocuments) => {
 const sort = (result, documents, invertedDocuments, terms) => {
     const collection = {};
     const countOfDocuments = documents.length;
-    console.log(JSON.stringify(countOfDocuments));
 
     for (let doc of documents) {
         collection[doc.id] = {};
@@ -62,27 +61,17 @@ const sort = (result, documents, invertedDocuments, terms) => {
 
     for (let term of terms) {
         for (let doc of documents) {
-            console.log(JSON.stringify(`term: ${term}`));
-            console.log(JSON.stringify(`doc: ${JSON.stringify(doc)}`));
-
             const tf = tfCalculate(term, doc, invertedDocuments);
             const idf = idfCalculate(countOfDocuments, term, invertedDocuments);
-
-            console.log(JSON.stringify(`tf: ${tf}`));
-            console.log(JSON.stringify(`idf: ${idf}`));
 
             collection[doc.id].INDEX += tf * idf;
         }
     }
 
-    console.log(JSON.stringify(collection));
-
     const answer = Object.entries(collection)
     .filter(entity => result.includes(entity[0]) && entity[1] !== 0)
     .sort((a,b)=> collection[b[0]].INDEX - collection[a[0]].INDEX)
     .map(entity => entity[0]);
-
-    console.log(JSON.stringify(`answer: ${answer}`));
 
     return answer;
 };
@@ -93,8 +82,6 @@ const tfCalculate = (term, doc, invertedDocuments) => {
 
 const idfCalculate = (countOfDocuments, term, invertedDocuments) => {
     const set = new Set(invertedDocuments[term]);
-    console.log(JSON.stringify(`countOfDocuments: ${countOfDocuments}`));
-    console.log(JSON.stringify(`set.size: ${set.size}`));
     return Math.log2(1 + (countOfDocuments - set.size + 1) / (set.size + 0.5));
 };
 
